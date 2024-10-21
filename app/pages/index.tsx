3282be1fc0763048d87b40e4bac26fd3c4d684ee
not-meet <mjmeet.279@gmail.com>
2024-10-21 11:06:37 +0530

    added the home page..

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,16 @@
+import { Vortex } from "@/components/ui/vortex";
+
 export default function Home() {
   return (
-    <h1>hello</h1>
+    <main>
+      <Vortex className="overflow-hidden h-[100vh] z-0" rangeY={250} rangeRadius={3} baseHue={30} particleCount={600}>
+        <div className="justify-center items-center flex flex-col gap-10 pt-20">
+          <h1 className="text-zinc-200 pt-36 shadow-black font-serif font-bold text-[8rem]" >Culinary Crate</h1>
+          <button className="px-4 py-2 rounded-xl border border-neutral-600 text-black bg-zinc-200 hover:bg-gray-200 text-lg transition duration-200">
+            Lets get started
+          </button>
+        </div>
+      </Vortex>
+    </main>
   );
 }
